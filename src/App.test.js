b2049fac76a83ca95242305ca22951778aa7c0f9
kxhcfk <reveersed@mail.ru>
2023-10-19 17:43:56 +0300

    test: simple render app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app components', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const roleEl = screen.getByRole('button');
+  const textEl = screen.getByText(/test/i);
+  expect(roleEl).toBeInTheDocument();
+  expect(textEl).toBeInTheDocument();
 });
