067e6e93e6daa48306ec73832e7e5cb9c99b803b
kxhcfk <reveersed@mail.ru>
2023-10-19 18:15:51 +0300

    test: renders app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [clicked, setClicked] = useState(false);
+    const [value, setValue] = useState('');
+
+    const toggle = () => setClicked(!clicked);
+
+    const change = (e) => setValue(e.target.value);
+
     return (
         <div className="App">
+            <div style={{color: 'red'}}>data</div>
             <h1>test</h1>
             <button>click</button>
-            <input type="text"/>
+            <input placeholder="value" value={value} onChange={change} type="text"/>
+            <div data-testid="value-elem">{value}</div>
+            <button data-testid="toggle-btn" onClick={toggle}>toggle</button>
+            {clicked ? <div data-testid="toggle-elem">toggle-elem</div> : null}
         </div>
     );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,47 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders app components', () => {
-  render(<App />);
-  const roleEl = screen.getByRole('button');
-  const textEl = screen.getByText(/test/i);
-  expect(roleEl).toBeInTheDocument();
-  expect(textEl).toBeInTheDocument();
-});
+describe('TEST APP', () => {
+    test('renders learn react elems', () => {
+        render(<App />);
+        const helloWorldElem = screen.getByText(/test/i)
+        const input = screen.getByPlaceholderText(/value/i)
+        expect(helloWorldElem).toBeInTheDocument();
+        expect(input).toMatchSnapshot();
+    });
+
+    test('renders learn react link', async() => {
+        render(<App />);
+        // const helloWorldElem = screen.queryByText(/hello2/i)
+        // expect(helloWorldElem).toBeNull()
+        screen.debug()
+        const helloWorldElem = await screen.findByText(/data/i)
+        expect(helloWorldElem).toBeInTheDocument();
+        expect(helloWorldElem).toHaveStyle({color: 'red'});
+        screen.debug()
+    });
+
+    test("CLICK EVENT", () => {
+        render(<App />);
+        const btn = screen.getByTestId('toggle-btn');
+        expect(screen.queryByTestId('toggle-elem')).toBeNull();
+        fireEvent.click(btn)
+        expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument();
+        fireEvent.click(btn)
+        expect(screen.queryByTestId('toggle-elem')).toBeNull();
+    })
+
+    test("INPUT EVENT", () => {
+        render(<App />);
+        const input = screen.getByPlaceholderText(/value/i)
+        expect(screen.queryByTestId('value-elem')).toContainHTML('');
+        // Искуственное событие
+        // fireEvent.input(input, {
+        //   target: {value: '123123'}
+        // })
+        // Близко к пользователю, обрабатываются события нажатия клавиш и тд
+        userEvent.type(input, '123123')
+        expect(screen.queryByTestId('value-elem')).toContainHTML('123123');
+    })
+})
